6b21da2ca712fd509b230ccdbff0e32fb08f5e0c
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-09-12 07:51:33 +0530

    Vince:Jenkins Project Repo

---
+++ src/test/java/jenkins/FinalJenkins_Execution.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class FinalJenkins_Execution
+{
+	@Test
+	public void executeCode()
+	{
+		String str="Vince"; // ecniV
+		System.out.println("Below is the output of Maven project Execution: "+str);
+
+	}
+
+}
