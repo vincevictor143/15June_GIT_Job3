3261b3f225fc20e728da5b95107b9bc30574f842
vincevictor143 <155403863+vincevictor143@users.noreply.github.com>
2024-09-20 08:20:18 +0530

    Update FinalJenkins_Execution.java
    Change 1

--- src/test/java/jenkins/FinalJenkins_Execution.java
+++ src/test/java/jenkins/FinalJenkins_Execution.java
@@ -9,6 +9,7 @@ public class FinalJenkins_Execution
 	{
 		String str="Vince"; // ecniV
 		System.out.println("Below is the output of Maven project Execution: "+str);
+		System.out.println("This change is happening for execution of Build trigger using poll SCM option");
 
 	}
 
